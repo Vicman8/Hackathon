2f80cf853b627794c77f107d8fca6b0c6808067d
riatmax <riatmax@gmail.com>
2024-04-13 12:41:13 -0400

    bunch o stuff

---
+++ Assets/Scripts/PlayerScripts/Penny.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penny : MonoBehaviour
+{
+    private float throwSpeed = 15;
+    private void Update()
+    {
+        transform.position = new Vector2(transform.position.x + (throwSpeed * Time.deltaTime), (-Mathf.Pow(transform.position.x, 2) + 15)/7);   
+    }
+}
